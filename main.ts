7d76c31c6b2ff58a97cd7dfeb9dee0645166a9cf
WoodenMaiden <yann.pomie@laposte.net>
2023-03-22 21:49:02 +0100

    feat: command
    Signed-off-by: WoodenMaiden <yann.pomie@laposte.net>

--- main.ts
+++ main.ts
@@ -1,20 +1,19 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, DropdownComponent, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
 type EventsTriggeringExpand = 'ON_SPACE' | 'ON_ENTER';
 
 const EventsTriggeringExpandMap = new Map<EventsTriggeringExpand, string>([
-	['ON_SPACE', 'Hitting Space'],
-	['ON_ENTER', 'Hitting Enter']
+	['ON_SPACE', 'When hitting Space'],
+	['ON_ENTER', 'When hitting Enter']
 ]); 
 
-interface MyPluginSettings {
+interface AbbreviationPluginSettings {
 	abbreviations: Record<string, string>;
 	eventsTriggeringExpand: EventsTriggeringExpand;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: AbbreviationPluginSettings = {
 	abbreviations: {
 		'e.g.': 'for example',
 		'atm' : 'at the moment',
@@ -22,79 +21,34 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 		'w/'  : 'with',
 		'w/o' : 'without',
 		'ily' : 'I love you',
-		'srv' : 'server',
 		'mvc' : 'Model View Controller',
-		'k8s' : 'Kubernetes'
+		'k8s' : 'Kubernetes',
+		'rs'  : 'Rust',
+		'js'  : 'JavaScript',
+		'ts'  : 'TypeScript',
+		'py'  : 'Python'
 	},
 	eventsTriggeringExpand: 'ON_SPACE',
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AbbreviationPlugin extends Plugin {
+	settings: AbbreviationPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
+		
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			id: 'choose-abbreviation-expansion-mode',
+			name: 'Choose how to expand abbrevations',
+			callback: () => {
+				new ChooseAbbreviationExpansionModal(this.app, this).open();
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new AbbreviationSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -110,14 +64,34 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class ChooseAbbreviationExpansionModal extends Modal {
+	plugin: AbbreviationPlugin;
+	
+	constructor(app: App, plugin: AbbreviationPlugin) {
 		super(app);
+		this.plugin = plugin;
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const center = contentEl.createEl('div', {cls: ".center"} );
+		
+		new DropdownComponent(center)
+			.addOptions(
+				Object.fromEntries(EventsTriggeringExpandMap)
+			)
+			.setValue(this.plugin.settings.eventsTriggeringExpand)
+			.onChange(async (value: EventsTriggeringExpand) => {
+				this.plugin.settings.eventsTriggeringExpand = value;
+				try {
+					await this.plugin.saveSettings();
+					new Notice('Settings changed ✅!');
+				} catch (error) {
+					new Notice('Error when saving settings! Check the logs');
+					console.error(error);
+				}
+				this.close()
+			})
 	}
 
 	onClose() {
@@ -126,19 +100,18 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class AbbreviationSettingTab extends PluginSettingTab {
+	plugin: AbbreviationPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: AbbreviationPlugin) {
 		super(app, plugin);
-		this.plugin = plugin;
 	}
 
 	display(): void {
 		const {containerEl} = this;
 
 		console.log('displaying settings tab', this.plugin.settings)
-
+		
 		containerEl.empty();
 
 		containerEl.createEl('h2', {text: 'Abbrevations Plugin - Settings'});
@@ -166,7 +139,7 @@ class SampleSettingTab extends PluginSettingTab {
 					.onClick(async () => {
 						console.log('add button clicked')
 						this.plugin.settings.abbreviations[''] = '';
-						await this.plugin.saveSettings();
+						this.plugin.saveSettings();
 						this.display();
 					})
 			)
